cbfdba5c261b2917eeccd3d5a764d6cc685f144c
Nazam <nazamalam@gmail.com>
2024-02-13 21:38:46 +0530

    Create store using reduxjs, reduxjs toolkit and redux-persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import {Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../app/user/userSlice';
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const {loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -21,7 +25,8 @@ export default function SignIn() {
     e.preventDefault();
 
     try{
-        setLoading(true);
+        // setLoading(true);
+        dispatch(signInStart())
       
       const res = await fetch('/api/user/signin', {
         method:'POST',
@@ -37,17 +42,20 @@ export default function SignIn() {
       e.target.reset();
       
       if(data.success === false) {
-        setLoading(false);
-        setError(data.Message);
+        // setLoading(false);
+        // setError(data.Message);
+        dispatch(signInFailure(data.Message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
 
     }catch(error){
-      setError(error.message);
-      setLoading(false)
+      // setError(error.message);
+      // setLoading(false)
+      dispatch(signInFailure(error.Message));
     }
   }
 
